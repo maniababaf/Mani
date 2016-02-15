5c02df1f6edb1372d38d93c26a434876d934b93a
maniababaf <maniababaf@users.noreply.github.com>
2016-02-15 19:30:35 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {157094408,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -235,10 +235,10 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+
+
+@freemani [Developer]
+@thisisparsa1382 [Manager]
 
 Special thanks to
 awkward_potato
@@ -247,8 +247,7 @@ topkecleon
 Vamptacus
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@free_bazaar [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
